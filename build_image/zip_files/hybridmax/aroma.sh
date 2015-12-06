0fae8d0c85e9a2b79b16b7e3176103a440c7db24
Hybridmax <hybridmax95@gmail.com>
2015-12-06 01:26:03 +0100

    build_image: Updated zip_files & META-INF

--- build_image/zip_files/hybridmax/aroma.sh
+++ build_image/zip_files/hybridmax/aroma.sh
@@ -1,14 +1,25 @@
 #!/sbin/sh
 
+# Aroma-Installer Kernel Tune Script
+#
+# Copyright (c) g.lewarne@xda
+#           (c) Hybridmax (hybridmax95@gmail.com)
+#           (c) thehacker911 (maik.diebenkorn@gmail.com)
+#
+# This Script based on g.lewarnes Script.
+# It was modified for my need.
+
+#vars
+PROP=/system/kernel.prop
+BP=/system/build.prop
+
 if [ "$1" = "WAIT" ]; then	
 	sleep 0.7
 elif [ "$1" = "FLASH" ]; then	
 	dd if=/tmp/boot.img of=/dev/block/platform/15570000.ufs/by-name/BOOT
 elif [ "$1" = "FIX_PERMS" ]; then	
-	chmod 644 /system/build.prop
-	chmod 755 /system/bin/cellgeofenced
-	chmod 755 /system/bin/rild
-	chmod 644 /system/kernel.prop
+	chmod 644 $BP
+	chmod 644 $PROP
 elif [ "$1" = "DETECT" ]; then	
 	# Probe /proc/cmdline to detect variant
 	if [ "`grep "G920T" /proc/cmdline`" != "" ]; then
@@ -58,156 +69,98 @@ elif [ "$1" = "DETECT" ]; then
 		fi
 	fi
 	sleep 0.7
-#elif [ "$1" = "PATCH" ]; then	
-#	if [ "`grep "G920T" /proc/cmdline`" != "" ]; then
-#		# found G920 T variant	
-#		cp /tmp/TLTE/etc/* /system/etc/
-#		cp -r /tmp/systemT/* /system/
-#	elif [ "`grep "G920W8" /proc/cmdline`" != "" ]; then
-#		# found G920 W8 variant	
-#		cp -r /tmp/systemT/* /system/
-#	elif [ "`grep "G925W8" /proc/cmdline`" != "" ]; then
-#		# found G925 W8 variant	
-#		cp -r /tmp/systemT/* /system/
-#	elif [ "`grep "G925T" /proc/cmdline`" != "" ]; then
-#		# found G925 T variant	
-#		cp /tmp/TLTE/etc/* /system/etc/
-#		cp -r /tmp/systemT/* /system/
-#	else
-#		# found international variant
-#		cp -r /tmp/systemF/* /system/
-#	fi
-#	chmod 0755 /system/bin/rild
-#	chmod 0755 /system/bin/cellgeofenced
 
-
-##### OPTIONAL CONFIGURATIONS START HERE
+# SCHEDULER
 elif [ "$1" = "CFQ" ]; then
-	echo "kernel.scheduler=cfq" >> /system/kernel.prop
+	echo "kernel.scheduler=cfq" >> $PROP
 
 elif [ "$1" = "BFQ" ]; then
-	echo "kernel.scheduler=bfq" >> /system/kernel.prop
+	echo "kernel.scheduler=bfq" >> $PROP
 
 elif [ "$1" = "DEADLINE" ]; then
-	echo "kernel.scheduler=deadline" >> /system/kernel.prop
+	echo "kernel.scheduler=deadline" >> $PROP
 
 elif [ "$1" = "FIOPS" ]; then
-	echo "kernel.scheduler=fiops" >> /system/kernel.prop
+	echo "kernel.scheduler=fiops" >> $PROP
 
 elif [ "$1" = "NOOP" ]; then
-	echo "kernel.scheduler=noop" >> /system/kernel.prop
+	echo "kernel.scheduler=noop" >> $PROP
 
+# GOVS
+elif [ "$1" = "CONSERVATIVE" ]; then
+	echo "kernel.governor=conservative" >> $PROP
+
+elif [ "$1" = "INTERACTIVE" ]; then
+	echo "kernel.governor=interactive" >> $PROP
+
+elif [ "$1" = "ONDEMAND" ]; then
+	echo "kernel.governor=ondemand" >> $PROP
+
+elif [ "$1" = "PERFORMANCE" ]; then
+	echo "kernel.governor=performance" >> $PROP
+sleep 0.7
+
+# CPU CLOCK A53
+elif [ "$1" = "A53-400" ]; then
+	echo "kernel.cpu.a53.min=400000" >> $PROP
+
+elif [ "$1" = "A53-1200" ]; then
+	echo "kernel.cpu.a53.max=1200000" >> $PROP
+
+elif [ "$1" = "A53-1296" ]; then
+	echo "kernel.cpu.a53.max=1296000" >> $PROP
+
+elif [ "$1" = "A53-1400" ]; then
+	echo "kernel.cpu.a53.max=1400000" >> $PROP
+
+elif [ "$1" = "A53-1500" ]; then
+	echo "kernel.cpu.a53.max=1500000" >> $PROP
+sleep 0.7
+
+# CPU CLOCK A57
+elif [ "$1" = "A57-800" ]; then
+	echo "kernel.cpu.a57.min=800000" >> $PROP
+
+elif [ "$1" = "A57-1704" ]; then
+	echo "kernel.cpu.a57.max=1704000" >> $PROP
+
+elif [ "$1" = "A57-1800" ]; then
+	echo "kernel.cpu.a57.max=1800000" >> $PROP
+
+elif [ "$1" = "A57-1896" ]; then
+	echo "kernel.cpu.a57.max=1896000" >> $PROP
+
+elif [ "$1" = "A57-2000" ]; then
+	echo "kernel.cpu.a57.max=2000000" >> $PROP
+
+elif [ "$1" = "A57-2100" ]; then
+	echo "kernel.cpu.a57.max=2100000" >> $PROP
+sleep 0.7
+
+# TURBO MODE
 elif [ "$1" = "TURBO" ]; then
-	echo "kernel.turbo=true" >> /system/kernel.prop
+	echo "kernel.turbo=true" >> $PROP
 
 elif [ "$1" = "NO_TURBO" ]; then
-	echo "kernel.turbo=false" >> /system/kernel.prop
+	echo "kernel.turbo=false" >> $PROP
 
-elif [ "$1" = "PERF_INTERACTIVE" ]; then
-	echo "kernel.interactive=performance" >> /system/kernel.prop
-
-elif [ "$1" = "BATT_INTERACTIVE" ]; then
-	echo "kernel.interactive=battery" >> /system/kernel.prop
-
-elif [ "$1" = "STOCK_INTERACTIVE" ]; then
-	echo "kernel.interactive=stock" >> /system/kernel.prop
-
-elif [ "$1" = "DHA_VM" ]; then
-	bp="/system/build.prop"
-	sed -i "/ro.config.sdha/d" $bp
-	sed -i "/ro.config.dha/d" $bp
-	sed -i "/ro.config.ldha/d" $bp
-	sed -i "/ro.config.fha/d" $bp
-	sed -i "/ro.sys.fw/d" $bp
-	sed -i "/kernel.vm/d" /system/kernel.prop
-	sed -i "/Kernel/d" $bp
-	echo "ro.config.dha_cached_max=16" >> $bp 
-	echo "ro.config.dha_cached_min=10" >> $bp
-	echo "ro.config.dha_empty_min=8" >> $bp
-	echo "ro.config.dha_empty_max=25" >> $bp
-	echo "ro.config.dha_lmk_scale=1" >> $bp
-	echo "ro.config.dha_th_rate=1" >> $bp
-	echo "kernel.vm=tuned" >> /system/kernel.prop
-
-elif [ "$1" = "FHA_VM" ]; then
-	bp="/system/build.prop"
-	sed -i "/ro.config.sdha/d" $bp
-	sed -i "/ro.config.dha/d" $bp
-	sed -i "/ro.config.ldha/d" $bp
-	sed -i "/ro.config.fha/d" $bp
-	sed -i "/ro.sys.fw/d" $bp
-	sed -i "/kernel.vm/d" /system/kernel.prop
-	sed -i "/Kernel/d" $bp
-	echo "ro.config.fha_enable=true" >> $bp
-	echo "kernel.vm=tuned" >> /system/kernel.prop
-
-elif [ "$1" = "STOCK_VM" ]; then
-	if [ "`grep "G920T" /proc/cmdline`" != "" ]; then
-		# found G920 T variant	
-		D=T
-	elif [ "`grep "G920W8" /proc/cmdline`" != "" ]; then
-		# found G920 W8 variant	
-		D=T
-	elif [ "`grep "G925T" /proc/cmdline`" != "" ]; then
-		# found G925 T variant	
-		D=T
-	elif [ "`grep "G925W8" /proc/cmdline`" != "" ]; then
-		# found G925 W8 variant	
-		D=T
-	elif [ "`grep "G920P" /proc/cmdline`" != "" ]; then
-		# found G920 P variant
-		D=P
-	elif [ "`grep "G925P" /proc/cmdline`" != "" ]; then
-		# found G925 P variant
-		D=P
-	elif [ "`grep "G920R" /proc/cmdline`" != "" ]; then
-		# found G925 R variant
-		D=P
-	elif [ "`grep "G925R" /proc/cmdline`" != "" ]; then
-		# found G925 R variant
-		D=P
-	else
-		# must be an international variant, assume F
-		D=F
-	fi
-	bp="/system/build.prop"
-	sed -i "/ro.config.sdha/d" $bp
-	sed -i "/ro.config.dha/d" $bp
-	sed -i "/ro.config.ldha/d" $bp
-	sed -i "/ro.config.fha/d" $bp
-	sed -i "/ro.sys.fw/d" $bp
-	sed -i "/kernel.vm/d" /system/kernel.prop
-	sed -i "/Kernel/d" $bp
-	#set stock device specific DHA - F variants are different to T variants
-	if [ "$D" = "F" ]; then
-		echo "ro.config.dha_cached_min=4" >> $bp
-		echo "ro.config.dha_empty_min=8" >> $bp
-		echo "ro.config.dha_lmk_scale=1.909" >> $bp
-	else
-		echo "ro.config.dha_cached_max=6" >> $bp
-		echo "ro.config.dha_cached_min=3" >> $bp
-		echo "ro.config.dha_empty_max=30" >> $bp
-		echo "ro.config.dha_empty_min=8" >> $bp
-		echo "ro.config.dha_lmk_scale=1.909" >> $bp
-		echo "ro.config.dha_th_rate=2.0" >> $bp
-	fi
-	echo "kernel.vm=stock" >> /system/kernel.prop
-
+# AUTOMATIC INITD CONTROL
 elif [ "$1" = "AUTO_INITD" ]; then
-	echo "kernel.initd=true" >> /system/kernel.prop
+	echo "kernel.initd=true" >> $PROP
 
 elif [ "$1" = "NO_INITD" ]; then
-	echo "kernel.initd=false" >> /system/kernel.prop
+	echo "kernel.initd=false" >> $PROP
 
+# GAPPS WAKELOCK FIX
 elif [ "$1" = "FIX_GAPPS" ]; then
-	echo "kernel.gapps=true" >> /system/kernel.prop
+	echo "kernel.gapps=true" >> $PROP
 
 elif [ "$1" = "STOCK_GAPPS" ]; then
-	echo "kernel.gapps=false" >> /system/kernel.prop
+	echo "kernel.gapps=false" >> $PROP
 
-###
-### ADD OTHER OPTIONS AS THEY ARE CREATED HERE
-###
+# REMOVE KNOX
+elif [ "$1" = "KNOX" ]; then
+	echo "kernel.knox=true" >> $PROP
 
 fi
 
